0b4ef6d9b58d87d904f0de57b5d441c021f9dde6
erikcas <erikcas1972@gmail.com>
2014-11-27 19:04:46 +0100

    lte bringup
    Change-Id: I191b5c96e9fbf7cb9978fdc9130df710aa10ee18

---
+++ omni_eagle_lte.mk
@@ -0,0 +1,11 @@
+# Inherit Omni GSM telephony parts
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Inherit Omni product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/sony/eagle/full_eagle.mk)
+
+PRODUCT_NAME := omni_eagle_lte
+PRODUCT_DEVICE := eagle
